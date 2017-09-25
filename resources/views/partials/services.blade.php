2765368963d56da647f22810e5e4503c8c5bb7b4
Suz Campbell <suz.blandon@gmail.com>
2017-09-25 16:06:05 -0600

    added footer. Added profile card pieces.

--- resources/views/partials/services.blade.php
+++ resources/views/partials/services.blade.php
@@ -5,37 +5,37 @@
 
     <div class="grid-x small-up-2 medium-up-3 large-up-6">
       <div class="cell">
-        <img class="thumbnail" src="http://placehold.it/300x400">
+        <img class="thumbnail" src="http://placekitten.com/300/400">
         <h5>Nulla At Nulla Justo, Eget</h5>
         <p>$400</p>
         <a href="#" class="button small expanded hollow">Buy</a>
       </div>
       <div class="cell">
-        <img class="thumbnail" src="http://placehold.it/300x400">
+        <img class="thumbnail" src="http://placekitten.com/300/400">
         <h5>Nulla At Nulla Justo, Eget</h5>
         <p>$400</p>
         <a href="#" class="button small expanded hollow">Buy</a>
       </div>
       <div class="cell">
-        <img class="thumbnail" src="http://placehold.it/300x400">
+        <img class="thumbnail" src="http://placekitten.com/300/400">
         <h5>Nulla At Nulla Justo, Eget</h5>
         <p>$400</p>
         <a href="#" class="button small expanded hollow">Buy</a>
       </div>
       <div class="cell">
-        <img class="thumbnail" src="http://placehold.it/300x400">
+        <img class="thumbnail" src="http://placekitten.com/300/400">
         <h5>Nulla At Nulla Justo, Eget</h5>
         <p>$400</p>
         <a href="#" class="button small expanded hollow">Buy</a>
       </div>
       <div class="cell">
-        <img class="thumbnail" src="http://placehold.it/300x400">
+        <img class="thumbnail" src="http://placekitten.com/300/400">
         <h5>Nulla At Nulla Justo, Eget</h5>
         <p>$400</p>
         <a href="#" class="button small expanded hollow">Buy</a>
       </div>
       <div class="cell">
-        <img class="thumbnail" src="http://placehold.it/300x400">
+        <img class="thumbnail" src="http://placekitten.com/300/400">
         <h5>Nulla At Nulla Justo, Eget</h5>
         <p>$400</p>
         <a href="#" class="button small expanded hollow">Buy</a>
