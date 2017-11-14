4a946eb09f36de0d7db162d80f570b7ece6940f5
Suz Campbell <suz.blandon@gmail.com>
2017-11-14 12:56:24 -0700

    adding the page link styles to simple image blade

--- resources/views/partials/masonry-cells/masonry-cell-only-image.blade.php
+++ resources/views/partials/masonry-cells/masonry-cell-only-image.blade.php
@@ -1,4 +1,15 @@
-@php ($image = get_sub_field('plain_image'))
-@if( $image )
-    {!! wp_get_attachment_image( $image, 'large') !!}
-@endif
+<div class="grid">
+<figure class="effect-ruby">
+<a href="{{ get_permalink($currentPost->ID) }}">
+    <figcaption>
+        @if( get_sub_field('page_link_text') )
+        <p>{{ the_sub_field('page_link_text') }}</p>
+        @endif
+    </figcaption>
+    @php ($image = get_sub_field('plain_image'))
+    @if( $image )
+        {!! wp_get_attachment_image( $image, 'large') !!}
+    @endif
+</a>
+</figure>
+</div>
