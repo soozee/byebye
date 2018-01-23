ac79b7410c1b94ac32dd054e563b9b55af02f276
Suz Campbell <suz.blandon@gmail.com>
2018-01-23 14:02:07 -0700

    fixing recent blog masonry tile

--- resources/views/partials/masonry-cells/masonry-cell-blog.blade.php
+++ resources/views/partials/masonry-cells/masonry-cell-blog.blade.php
@@ -3,7 +3,9 @@
     @php($mostRecentPost = get_posts(['numberposts' => 1, 'post_type' => 'post']))
     @foreach ($mostRecentPost as $recent)
   <a href="{{ get_permalink($recent->ID) }}">
-    <h3>{{ get_the_title($recent->ID) }}</h3>
+    <figcaption>
+        <h3>{{ get_the_title($recent->ID) }}</h3>
+    </figcaption>
   </a>
   <a href="{{ get_permalink($recent->ID) }}">
     {!! get_the_post_thumbnail($recent->ID) !!}
